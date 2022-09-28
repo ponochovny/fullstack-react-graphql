dd8d7f2348fe00b5c3271be52e92881514f33b7c
ponochovny <ponochovny@gmail.com>
2022-09-28 21:10:50 +0300

    backend auth page, user schema

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,11 @@
+import { User } from './schemas/User'
 import { config, createSchema } from '@keystone-next/keystone/schema'
 import 'dotenv/config'
+import { createAuth } from '@keystone-next/auth'
+import {
+	withItemData,
+	statelessSessions,
+} from '@keystone-next/keystone/session'
 
 const databaseURL =
 	process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial'
@@ -9,19 +15,35 @@ const sessionConfig = {
 	secret: process.env.COOKIE_SECRET,
 }
 
-export default config({
-	server: {
-		cors: {
-			origin: [process.env.FRONTEND_URL],
-			credentials: true,
-		},
-	},
-	db: {
-		adapter: 'mongoose',
-		url: databaseURL,
-	},
-	lists: createSchema({}),
-	ui: {
-		isAccessAllowed: () => true,
+const { withAuth } = createAuth({
+	listKey: 'User',
+	identityField: 'email',
+	secretField: 'password',
+	initFirstItem: {
+		fields: ['name', 'email', 'password'],
 	},
 })
+
+export default withAuth(
+	config({
+		server: {
+			cors: {
+				origin: [process.env.FRONTEND_URL],
+				credentials: true,
+			},
+		},
+		db: {
+			adapter: 'mongoose',
+			url: databaseURL,
+		},
+		lists: createSchema({
+			User,
+		}),
+		ui: {
+			isAccessAllowed: ({ session }) => !!session?.data,
+		},
+		session: withItemData(statelessSessions(sessionConfig), {
+			User: `id name email`,
+		}),
+	})
+)
