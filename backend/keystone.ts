7bec70b3019a9e3691d9d67ae81ea3a0ff5d2970
ponochovny <ponochovny@gmail.com>
2022-10-14 20:22:28 +0300

    Related Product Image

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -6,6 +6,7 @@ import {
 } from '@keystone-next/keystone/session'
 import { User } from './schemas/User'
 import { Product } from './schemas/Product'
+import { ProductImage } from './schemas/ProductImage'
 import 'dotenv/config'
 
 const databaseURL =
@@ -40,6 +41,7 @@ export default withAuth(
 		lists: createSchema({
 			User,
 			Product,
+			ProductImage
 		}),
 		ui: {
 			isAccessAllowed: ({ session }) => !!session?.data,
